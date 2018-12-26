71dacfd8b1fcdd8efb54a15a24e1540ca1326f6b
Nadine <nadioliv@gmail.com>
2018-12-26 19:21:36 +0000

    Corrige conflitos

--- tablespaces_datafiles.sql
+++ tablespaces_datafiles.sql
@@ -5,7 +5,8 @@ CREATE TEMPORARY TABLESPACE aebd_trabalho_temp
     TEMPFILE '\u01\app\oracle\oradata\orcl12\orcl\aebd_trabalho_temp01.dbf' 
     SIZE 250M
     AUTOEXTEND on;    
-    
+CREATE USER jjnm IDENTIFIED BY oracle;
+
 GRANT RESOURCE TO jjnm;
 GRANT CONNECT TO jjnm;
 GRANT DBA TO jjnm;
